f92c9b999b1f47fd41359373b5c12b1fe63c72cc
Roman Levin <romanlevin@gmail.com>
2014-08-31 22:44:00 +0200

    Initial commit

---
+++ Assets/scripts/PickupFactory.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupFactory : MonoBehaviour {
+
+	public int numberOfPickups = 10;
+	public float spreadRadius = 3f;
+	public GameObject pickupObject;
+
+	void DestroyStartingPickups(){
+		GameObject[] startingPickups = GameObject.FindGameObjectsWithTag ("Pickup");
+		
+		foreach (GameObject startingPickup in startingPickups) {
+			Destroy(startingPickup);
+		}
+	}
+
+	Vector3[] GeneratePickupPoints(){
+		Vector3[] pickupPoints = new Vector3[numberOfPickups];
+		float angle;
+		float angleFraction = 2f * Mathf.PI / numberOfPickups;
+		for (int i = 0; i < numberOfPickups; i++) {
+			angle = angleFraction * i;
+			pickupPoints[i] = new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin (angle)) * spreadRadius + Vector3.up;
+		}
+		return pickupPoints;
+	}
+
+	void GeneratePickups(){
+		Vector3[] pickupPoints = GeneratePickupPoints();
+		foreach (Vector3 pickupPoint in pickupPoints) {
+			Instantiate(pickupObject, pickupPoint, Quaternion.identity);
+		}
+	}
+
+	void Start () {
+		if (numberOfPickups > 0) {
+			DestroyStartingPickups ();
+		}
+		GeneratePickups ();
+	}
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float forceFactor;
+	private int count;
+	private Vector3 targetScale;
+	private Vector3 scaleVelocity = Vector3.zero;
+	public GUIText countText;
+	public GUIText winText;
+
+	void Start() {
+		count = 0;
+		UpdateCount ();
+		winText.text = "";
+		targetScale = transform.localScale;
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		AdjustScale ();
+		
+		Vector3 force = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rigidbody.AddForce (force * forceFactor * Time.deltaTime);
+	}
+
+	private void UpdateCount() {
+		countText.text = "Count: " + count.ToString ();
+	}
+
+	private void PickUp(GameObject pickup) {
+		pickup.SetActive(false);
+		count += 1;
+		targetScale += Vector3.one * 0.2f;
+		UpdateCount ();
+		if (GameObject.FindGameObjectsWithTag ("Pickup").Length == 0) {
+			winText.text = "A Winner is You!";
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Pickup") {
+			PickUp(other.gameObject);
+		}
+	}
+
+	private void AdjustScale() {
+		if (targetScale != transform.localScale) {
+			transform.localScale = Vector3.SmoothDamp(transform.localScale, targetScale, ref scaleVelocity, 0.3f);
+		}
+	}
+}
