cc53f58e2048df6fa8021796bd591481fcc13841
Roman Levin <romanlevin@gmail.com>
2014-09-01 20:50:39 +0200

    Clean up Rotator.cs

--- Assets/scripts/Rotator.cs
+++ Assets/scripts/Rotator.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Rotator : MonoBehaviour {
+public class Rotator : MonoBehaviour
+{
 	
 
-	// Update is called once per frame
-	void Update () {
-		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
-	}
+		// Update is called once per frame
+		void Update ()
+		{
+				transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		}
 }
